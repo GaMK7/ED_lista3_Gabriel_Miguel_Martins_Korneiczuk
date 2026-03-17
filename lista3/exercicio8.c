03ee9809e6fcace012416702697873a346551f6b
GaMK7 <gabrielmiguelmartins@gmail.com>
2026-03-17 14:22:44 -0400

    Create exercicio8.c

---
+++ lista3/exercicio8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int valores[6];
+
+    for (int i = 0; i < 6; i++) {
+        printf("Valor %d: ", i + 1);
+        scanf("%d", &valores[i]);
+    }
+
+    printf("Ordem inversa: ");
+    for (int i = 5; i >= 0; i--) {
+        printf("%d ", valores[i]);
+    }
+
+    return 0;
+}
